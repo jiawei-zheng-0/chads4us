e1ce6e2037fdbcf359e1e2ac86a6c3697a51d199
Jia Wei Zheng <jiasterop@gmail.com>
2020-03-25 15:49:23 -0400

    add login func

--- server.js
+++ server.js
@@ -9,21 +9,12 @@ const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
 const { Pool, Client } = require('pg')
 const config = require('./config.json');
+const db = require('./db.js');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-const userDB = new Pool({
-    user: config.userDBusername,
-    host: config.userDBhost,
-    database: config.userDBdatabase,
-    password: config.userDBpassword,
-    port: config.userDBport,
-    ssl: true
-})
-console.log(config.userDBusername);
 
 app.use(session({
     key: 'user_sid',
@@ -49,17 +40,17 @@ let sessionChecker = (req, res, next) => {
     }
 };
 
-app.get('/', sessionChecker, function (req, res) {
+app.get('/', sessionChecker, (req, res) => {
     res.sendFile(__dirname + "/public/" + "index.html");
 })
 
-app.post('/posttest', function (req, res) {
+app.post('/posttest', (req, res) => {
     res.status(200).send({
         status: "post response"
     });
 })
 
-app.get('/gettest', function (req, res) {
+app.get('/gettest', (req, res) => {
     res.status(200).send({
         status: "get response"
     });
@@ -67,30 +58,43 @@ app.get('/gettest', function (req, res) {
 
 //API
 
-app.post('/register', function (req, res) {
+app.post('/register', (req, res) => {
     let username = req.body.username;
     let password = req.body.password;
     console.log(`username is ${username}`);
     console.log(`password is ${password}`);
-    bcrypt.hash(password, 10, function (err, hash) {
+    bcrypt.hash(password, 10, (err, hash) => {
         console.log(`pw hash is ${hash}`);
-        userDB.query('INSERT INTO users(userid,password) VALUES($1, $2)', [username, hash], (error, results) => {
-            if (error) {
-                console.log(error);
+        db.register(username, hash, (err, result) => {
+            if (err) {
+                console.log('Username already exists');
                 res.status(500).send();
             }
-            res.status(200).send();
-          })
-
+            else {
+                res.status(200).send();
+            }
+        });
     });
 })
 
 app.post('/login', function (req, res) {
     let username = req.body.username;
     let password = req.body.password;
-
-    res.status(200).send({
-        status: "post response"
+    db.login(username, (err, hashedPW) => {
+        if (err) {
+            console.log('Username does not exist');
+            res.status(500).send();
+        }
+        else {
+            bcrypt.compare(password, hashedPW, (err, result) => {
+                if (result) {
+                    res.status(200).send();
+                } else {
+                    console.log('Wrong password');
+                    res.status(500).send();
+                }
+            });
+        }
     });
 })
 
