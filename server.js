02df69f31ad901ed64dd23cec58f4cb277ae98a9
Jia Wei Zheng <jiasterop@gmail.com>
2020-03-25 00:38:41 -0400

    Add hashing for passwords during registration

--- server.js
+++ server.js
@@ -1,32 +1,99 @@
-var express = require('express');
-var fs = require("fs");
-var app = express();
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var path = require('path');
-var urlencodedParser = bodyParser.urlencoded({ extended: true })
+const express = require('express');
+const fs = require("fs");
+const app = express();
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
+const urlencodedParser = bodyParser.urlencoded({ extended: true })
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const { Pool, Client } = require('pg')
+const config = require('./config.json');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get('/', function (req, res) {
+const userDB = new Pool({
+    user: config.userDBusername,
+    host: config.userDBhost,
+    database: config.userDBdatabase,
+    password: config.userDBpassword,
+    port: config.userDBport,
+    ssl: true
+})
+console.log(config.userDBusername);
+
+app.use(session({
+    key: 'user_sid',
+    secret: 'secretstuff',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}));
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');
+    }
+    next();
+});
+//if user is already logged in
+let sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.redirect('/public/index.html');
+    } else {
+        next();
+    }
+};
+
+app.get('/', sessionChecker, function (req, res) {
     res.sendFile(__dirname + "/public/" + "index.html");
 })
 
 app.post('/posttest', function (req, res) {
-	res.status(200).send({
-                status: "post response"
+    res.status(200).send({
+        status: "post response"
     });
 })
 
 app.get('/gettest', function (req, res) {
-	res.status(200).send({
-                status: "get response"
+    res.status(200).send({
+        status: "get response"
     });
 })
 
 //API
+
+app.post('/register', function (req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+    console.log(`username is ${username}`);
+    console.log(`password is ${password}`);
+    bcrypt.hash(password, 10, function (err, hash) {
+        console.log(`pw hash is ${hash}`);
+        userDB.query('INSERT INTO users(userid,password) VALUES($1, $2)', [username, hash], (error, results) => {
+            if (error) {
+                console.log(error);
+                res.status(500).send();
+            }
+            res.status(200).send();
+          })
+
+    });
+})
+
+app.post('/login', function (req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    res.status(200).send({
+        status: "post response"
+    });
+})
+
 /*
 app.post('/login', function (req, res) {
 	res.status(500).send({
@@ -63,5 +130,5 @@ app.post('/login', function (req, res) {
 })
 */
 const PORT = process.env.PORT || 5000
-var server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+var server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
