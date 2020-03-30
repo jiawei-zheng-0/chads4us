5fa5aaebc1904cd2242aba7d22892974a1227351
Jia Wei Zheng <jiasterop@gmail.com>
2020-03-29 23:09:14 -0400

    Add front end test 1

---
+++ public/precache-manifest.1b15a9e37ee23805c20f677945011102.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "785eea8185b465915260cd7468510ab9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e60f26f40309adc86da",
+    "url": "/static/css/main.d579de68.chunk.css"
+  },
+  {
+    "revision": "3bd0a733cdec9f83a96a",
+    "url": "/static/js/2.db4117df.chunk.js"
+  },
+  {
+    "revision": "7c0cf88284ae3ee844ea98d949191f75",
+    "url": "/static/js/2.db4117df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e60f26f40309adc86da",
+    "url": "/static/js/main.2c9aa7a3.chunk.js"
+  },
+  {
+    "revision": "45344d58f9a01c999fd5",
+    "url": "/static/js/runtime-main.edae25c4.js"
+  },
+  {
+    "revision": "8b89fa846b654171299ab279069b983a",
+    "url": "/static/media/abstrakt-design-03.8b89fa84.png"
+  },
+  {
+    "revision": "91f94de979538734ed3ccb2f41c4d418",
+    "url": "/static/media/book.91f94de9.png"
+  },
+  {
+    "revision": "d1f917bed3874c68829769549b66d1d2",
+    "url": "/static/media/college.d1f917be.jpg"
+  },
+  {
+    "revision": "d37e36075403d4eda3a33ec0b8983f04",
+    "url": "/static/media/college.d37e3607.png"
+  },
+  {
+    "revision": "f21dceb6e5a41633aeb6284a656f0bde",
+    "url": "/static/media/herringbone.f21dceb6.png"
+  },
+  {
+    "revision": "beb1c65ee48dfdb0a3d6408d60a2e239",
+    "url": "/static/media/ralph.beb1c65e.jpg"
+  },
+  {
+    "revision": "5d2138695af1bc1b4aa26982ad9f97d5",
+    "url": "/static/media/spikes.5d213869.png"
+  },
+  {
+    "revision": "eccae564ad029a6c16d99bab5a737f63",
+    "url": "/static/media/women_hand.eccae564.png"
+  }
+]);
