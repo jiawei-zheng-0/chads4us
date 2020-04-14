341ee7f06ae254b1a3e2d8af276d57a16bf3ebc5
Jia Wei Zheng <jiasterop@gmail.com>
2020-04-13 23:25:46 -0400

    code 7 front end update

--- public/precache-manifest.9868d08e2e42c14827208bdabc65d5fe.js
+++ public/precache-manifest.9868d08e2e42c14827208bdabc65d5fe.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "83ebd7f533d410d012dd0e3cd8c82826",
+    "revision": "859503f8b42d27b7499e434e3f75a326",
     "url": "/index.html"
   },
   {
-    "revision": "c2b53a129bb984368b9b",
-    "url": "/static/css/main.d579de68.chunk.css"
+    "revision": "8d696b51a7141ec46bfa",
+    "url": "/static/css/main.8b0fe3de.chunk.css"
   },
   {
-    "revision": "3bd0a733cdec9f83a96a",
-    "url": "/static/js/2.db4117df.chunk.js"
+    "revision": "ecd76ed879908899b693",
+    "url": "/static/js/2.2a5ec93d.chunk.js"
   },
   {
     "revision": "7c0cf88284ae3ee844ea98d949191f75",
-    "url": "/static/js/2.db4117df.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.2a5ec93d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c2b53a129bb984368b9b",
-    "url": "/static/js/main.aca8422f.chunk.js"
+    "revision": "8d696b51a7141ec46bfa",
+    "url": "/static/js/main.1053f532.chunk.js"
   },
   {
     "revision": "45344d58f9a01c999fd5",
@@ -26,6 +26,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "8b89fa846b654171299ab279069b983a",
     "url": "/static/media/abstrakt-design-03.8b89fa84.png"
+  },
+  {
+    "revision": "724be14702c88f0b10c99e0bc62b8e95",
+    "url": "/static/media/bg2.724be147.png"
   },
   {
     "revision": "91f94de979538734ed3ccb2f41c4d418",
@@ -46,6 +50,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "beb1c65ee48dfdb0a3d6408d60a2e239",
     "url": "/static/media/ralph.beb1c65e.jpg"
+  },
+  {
+    "revision": "abb607bafd7566cf11598f2d0f7d66ea",
+    "url": "/static/media/schoolAppLogo.abb607ba.png"
   },
   {
     "revision": "5d2138695af1bc1b4aa26982ad9f97d5",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17c385e702226d4abeae2efd2d413703.js"
+  "/precache-manifest.9868d08e2e42c14827208bdabc65d5fe.js"
 );
 
 self.addEventListener('message', (event) => {
